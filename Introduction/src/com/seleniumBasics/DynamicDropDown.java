1f9b8af0d957b33ebefbb48a1e81e301a52fd89b
vsn29 <vsnlearn29@gmail.com>
2020-05-24 23:23:41 +0530

    Handling AutoSuggestive Drop Downs in selenium

--- Introduction/src/com/seleniumBasics/DynamicDropDown.java
+++ Introduction/src/com/seleniumBasics/DynamicDropDown.java
@@ -1,10 +1,6 @@
 //Handling Dynamic Drop Down in selenium
 //=====================================================================
 
-//Using Select class for selecting value from dropdown  
-//Select dropdown = new Select(driver.findElement(By.id("testingDropdown")));  
-//dropdown.selectByVisibleText("Database Testing");
-
 package com.seleniumBasics;
 
 import org.openqa.selenium.By;
@@ -23,16 +19,28 @@ public class DynamicDropDown {
 		WebDriver driver = new ChromeDriver();
 
 		// Launch Website
-		driver.navigate().to("https://www.testandquiz.com/selenium/testing.html");
+		driver.navigate().to("http://spicejet.com");
 
 		driver.manage().window().maximize();
 
 		Thread.sleep(5000);
 
-//Using Select class for selecting value from dropdown  
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 
-		Select dropdown = new Select(driver.findElement(By.id("testingDropdown")));
-		dropdown.selectByVisibleText("Database Testing");
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		
+		Thread.sleep(2000);
+
+		// driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']"))
+				.click();
+		Thread.sleep(2000);
+
+		/*
+		 * driver.findElement(By.cssSelector(
+		 * ".ui-state-default.ui-state-highlight.ui-state-active")).click();
+		 */
 
 		Thread.sleep(2000);
 		// Close the Browser
